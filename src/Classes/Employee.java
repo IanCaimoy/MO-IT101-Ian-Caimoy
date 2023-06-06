a8dad929293e840f700abc8738cbfbd142566f99
Ian Kent <Ian Kent@192.168.0.30>
2023-06-06 10:16:50 +0800

    Net Salary Computation - System User Input

--- src/Classes/Employee.java
+++ src/Classes/Employee.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package motorph;
+package Classes;
 import java.time.LocalTime;
 import java.time.Duration;
 
